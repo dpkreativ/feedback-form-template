878217919aa13fbba997a70cdcf90e3e0f5da223
Divine Orji <dpkreativ@gmail.com>
2023-01-17 08:19:02 +0100

    Start afresh with Next 13

--- components/layouts/HomeLayout.js
+++
@@ -1,9 +0,0 @@
-export default function HomeLayout({ children }) {
-  return (
-    <div className="min-h-screen bg-[#E0E9F4]">
-      <main className="flex items-center justify-center px-5 py-20">
-        {children}
-      </main>
-    </div>
-  );
-}
