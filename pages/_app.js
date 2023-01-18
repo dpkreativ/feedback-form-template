bdec75ed47686c4475a5a982d4d6900347293131
Divine Orji <dpkreativ@gmail.com>
2023-01-18 01:34:47 +0100

    Revert to boilerplate code

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,5 @@
-import { NextUIProvider } from '@nextui-org/react';
+import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return (
-    <NextUIProvider>
-      <Component {...pageProps} />
-    </NextUIProvider>
-  );
+  return <Component {...pageProps} />
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document';
-import { CssBaseline } from '@nextui-org/react';
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>{CssBaseline.flush()}</Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
