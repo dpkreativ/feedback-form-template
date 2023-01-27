b18e474cf6a0f08b36e903aeb42c611c3c86e0cb
Divine Orji <dpkreativ@gmail.com>
2023-01-27 15:53:32 +0100

    feedback prompt

---
+++ components/ui/FeedbackForm.js
@@ -0,0 +1,9 @@
+export default function FeedbackForm() {
+  return (
+    <div>
+      <form>
+        <label htmlFor="">First Name</label>
+      </form>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import HomeLayout from '@/components/layouts/HomeLayout';
+import FeedbackForm from '@/components/ui/FeedbackForm';
 import Head from 'next/head';
 
 export default function Home() {
@@ -9,7 +11,23 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>Hi Divi!</main>
+      <HomeLayout>
+        <section className="grid md:grid-cols-12">
+          <div className="bg-violet-50 md:col-span-5">
+            <div className="p-10 md:p-20 flex flex-col space-y-10 md:space-y-20">
+              <h1 className="text-5xl font-bold leading-snug text-violet-600">
+                Feel free to drop us your feedback.
+              </h1>
+              <div>
+                <img src="/feedback.svg" alt="Feedback" />
+              </div>
+            </div>
+          </div>
+          <div className="md:col-span-7">
+            <FeedbackForm />
+          </div>
+        </section>
+      </HomeLayout>
     </>
   );
 }
